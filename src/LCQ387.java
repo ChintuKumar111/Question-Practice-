256262880b82881044360203cd7e7123d68d612a
chintukumar111 <chintudas499@gmail.com>
2025-07-26 23:53:17 +0530

    revised concept

--- src/LCQ387.java
+++ src/LCQ387.java
@@ -1,34 +1,29 @@
 public class LCQ387 {
-    public static void main(String[] args) {
 
-        String str = "aabb";
-        boolean found = false;
+        public int firstUniqChar(String s) {
 
-        for (int i = 0; i < str.length(); i++) {
-            boolean bn = true;
-            for (int j = 0; j < str.length(); j++) {
+            for(int i = 0;i<s.length();i++){
 
-                if (i != j && str.charAt(i) == str.charAt(j)) {
+                boolean unique = true;
 
-                    bn = false;
-                    break;
+                for(int j = 0 ;j<s.length();j++){
 
-
+                    if(i!=j && s.charAt(i)==s.charAt(j)){
+                        unique = false;
+                        break;
+                    }
                 }
+                if(unique)
+                    return i;
             }
+            return -1;
+        }
 
-            if (bn) {
-                System.out.println(i + "index");
-                found = true;
-                break;
+        public static void main(String []args){
 
-            }
+           LCQ387 l1 = new LCQ387();
+            System.out.println(l1.firstUniqChar("loveleetcode"));
 
         }
-        if(!found)System.out.println("-1");
-
-
-
     }
 
-}
