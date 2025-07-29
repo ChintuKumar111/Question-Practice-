15a83af4ccfc8cad745aca0310bb6351de8c208f
chintukumar111 <chintudas499@gmail.com>
2025-07-29 18:49:41 +0530

    solving lcq question

--- src/LCQ1832.java
+++ src/LCQ1832.java
@@ -8,15 +8,15 @@ public class LCQ1832 {
 
         int count = 0;
 
-        learning l = new learning();
-        String s= "thequickbrownfxjum";
+        LCQ1832 l = new LCQ1832();
+        String s= "quick brown fox jumps over the lazy dog";
 
         System.out.println(l.have(s));
 
 
     }
 
-    boolean have(String s) {
+     public boolean have(String s) {
 
         String str = s;
 
--- src/learning.java
+++ src/learning.java
@@ -1,41 +1,121 @@
+import java.io.RandomAccessFile;
+import java.sql.SQLOutput;
+
 public class learning {
     public static void main(String[] args) {
 
+//
+//        int count = 0;
+//
+//learning l = new learning();
+//String s= "thequickbrownfxjum";
+//
+//        System.out.println(l.have(s));
+//
+//
+//    }
+//
+//    boolean have(String s) {
+//
+//        String str = s;
+//
+//        int count = 0;
+//
+//
+//        for (char i = 'a'; i <= 'z'; i++) {
+//
+//            for (int j = 0; j < str.length(); j++) {
+//
+//                if (i == str.charAt(j)) {
+//
+//                    count++;
+//                    break;
+//                }
+//
+//
+//            }
+//        }
+//        if(count==26){
+//            return true;
+//        }
+//
+//return false;
 
-        int count = 0;
 
-learning l = new learning();
-String s= "thequickbrownfxjum";
-
-        System.out.println(l.have(s));
+        String str[] = {"This is the Levi Ackerman"};
 
 
-    }
-
-    boolean have(String s) {
-
-        String str = s;
-
-        int count = 0;
+        int arr[] = new int[0];
 
 
-        for (char i = 'a'; i <= 'z'; i++) {
+        for(String d : str){
 
-            for (int j = 0; j < str.length(); j++) {
+            arr = new int[d.length()];
+        }
 
-                if (i == str.charAt(j)) {
+        int x = 0;
 
-                    count++;
-                    break;
-                }
+
+
+
+
+        for(String v : str){
+            System.out.println(v);
+            int count = 0 ;
+            int value = 0;
+            for(int i = 0; i< v.length(); i++) {
+
+
+            char c = v.charAt(i);
+
+            if (c != ' ') {
+                count++;
+                value= count;
+                arr[x++] = value;
 
 
             }
-        }
-        if(count==26){
-            return true;
-        }
 
-return false;
+            if (c == ' ') {
+                count=0;
+
+            }
+
+
+
+            }
+
+            System.out.println("word length = " + count);
+            System.out.println("Final value = " + value);
+
+
+        }
+        int max = arr[0];
+
+        for(int i = 0;i< arr.length;i++){
+
+            if(max<arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println("max value is "+max);
+
+
+
+
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
