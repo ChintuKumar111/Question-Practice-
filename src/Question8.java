e3d0e9ec857018c60fb758364a8ad339f62128c6
chintukumar111 <chintudas499@gmail.com>
2025-08-02 01:29:43 +0530

    find-first-palindromic-string-in-the-array

---
+++ src/Question8.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static void main(String[] args) {
+
+        String[] s = {"abc", "car", "ada", "racecar", "cool"};
+
+
+
+
+        ArrayList<String> ar = new ArrayList<>();
+
+        for(String ec : s){
+            ar.add(ec);
+
+            String reverse = new StringBuilder(ec).reverse().toString();
+
+            if (reverse.equals(ec)) {
+                System.out.println(ec);
+                return;
+            }
+        }
+    }
+
+
+
+}
