a9be374039a83e48d57e9c29885d31f71fa5b61a
chintukumar111 <chintudas499@gmail.com>
2025-08-11 10:51:44 +0530

    ..

---
+++ src/Experiment/Cg.java
@@ -0,0 +1,21 @@
+package Experiment;
+
+public class Cg {
+
+    public static  void main (String args[]){
+
+
+        int n = 4;
+        int m = 3;
+        int result = 0;
+
+        for (int i = 1;i<=m;i++){
+
+            result+=n;
+
+
+
+        }
+        System.out.println(result);
+    }
+}
