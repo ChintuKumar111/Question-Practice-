7286d75619d037418b8914d49596d55b117a1eeb
chintukumar111 <chintudas499@gmail.com>
2025-08-22 23:35:17 +0530

    searching in string array

---
+++ src/Experiment/CountNumberPresentInStringArray.java
@@ -0,0 +1,21 @@
+package Experiment;
+
+public class CountNumberPresentInStringArray {
+    public static void main(String[] args) {
+
+
+        int s [] = {1,2,3,4,5,6,1,2,3,4,5,2,3,43,2,2,2,3,3,1,1,2,3,5,6,7};
+
+        for(int i = 0 ;i <50;i++){
+            int count = 0;
+            for(int j = 0 ; j< s.length;j++){
+
+                if(i == s[j]){
+                    count++;
+
+                }
+            }
+            System.out.println(i +" = "+count);
+        }
+    }
+}
--- src/Experiment/sortArayd.java
+++ src/Experiment/sortArayd.java
@@ -1,2 +1,22 @@
-package Experiment;public class sortArayd {
-}
+package Experiment;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class sortArayd {
+
+
+
+        public static void main(String[] args) {
+            Integer[] numbers = {5, 3, 8, 1, 2}; // Use Integer (not int) for Collections.reverseOrder()
+
+            System.out.println("Original Array: " + Arrays.toString(numbers));
+
+            // Sorting in descending order
+            Arrays.sort(numbers, Collections.reverseOrder());
+
+            System.out.println("Sorted Array (Descending): " + Arrays.toString(numbers));
+        }
+    }
+
+
