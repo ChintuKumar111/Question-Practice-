c2a905474667dbfe1da4ab62806671df4793c6a8
chintukumar111 <chintudas499@gmail.com>
2025-08-22 23:36:11 +0530

    present count

--- src/Experiment/CountNumberPresentInStringArray.java
+++ src/Experiment/CountNumberPresentInStringArray.java
@@ -3,7 +3,7 @@ package Experiment;
 public class CountNumberPresentInStringArray {
     public static void main(String[] args) {
 
-
+// number of count in present in array
         int s [] = {1,2,3,4,5,6,1,2,3,4,5,2,3,43,2,2,2,3,3,1,1,2,3,5,6,7};
 
         for(int i = 0 ;i <50;i++){
